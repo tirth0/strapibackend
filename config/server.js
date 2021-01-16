ffa1da4d13d46f9b0706f7b39ad025b315ba9c50
tirth0 <tirtharajsengupta@gmail.com>
2021-01-16 13:11:16 +0530

    strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '241ecdd465812572601d4a9105a09a7f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccb14bf8-5795-4fe0-8370-0aff063bfa8e'
+};
